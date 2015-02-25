2f0f8c375eff1f40ce74db3c2de91c2458af778d
tanmayAccela <tthakur@accela.com>
2015-02-25 12:35:04 -0800

    Changed setup.py file

--- default/src/ckanext-usmetadata_demo/setup.py
+++ default/src/ckanext-usmetadata_demo/setup.py
@@ -6,13 +6,13 @@ version = '1.0'
 setup(
     name='ckanext-usmetadata_demo',
     version=version,
-    description="demo of USmetadata extension",
+    description="CivicData compatible version of USmetadata extension",
     long_description='''
     ''',
     classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
     keywords='',
     author='tanmay',
-    author_email='',
+    author_email='tthakur@accela.com',
     url='',
     license='',
     packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
